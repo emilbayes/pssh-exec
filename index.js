742a1c1293dd723e45468a1968b2f10562789a00
Emil Bay <github@tixz.dk>
2016-03-06 13:49:00 +0100

    Fix mulit-duplex-stream

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var ssh = require('ssh-exec')
 var eos = require('end-of-stream')
 var objectAssign = require('object-assign')
-var multiDuplexStream = require('./multi-duplex-stream')
+var multiDuplexStream = require('multi-duplex-stream')
 
 module.exports = function PsshExec (cmd, hosts, opts, cb) {
   if (!(this instanceof PsshExec)) return new PsshExec(cmd, hosts, opts, cb)
